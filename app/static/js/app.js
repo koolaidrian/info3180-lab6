27dd595f3e4de0c2c930107fa85d11094b78fd7e
koolaidrian <chandap2011@gmail.com>
2020-04-26 23:41:34 -0500

    lab 6

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -10,11 +10,11 @@ Vue.component('app-header', {
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <li class="nav-item active">     
+		    <router-link to = "/" class = "nav-link">Home</router-link> 
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">News</a>
+		    <router-link to = "/news" class = "nav-link">News</router-link> 
                   </li>
                 </ul>
               </div>
@@ -41,11 +41,104 @@ Vue.component('app-footer', {
     }
 })
 
+const NewsList = Vue.component('news-list',{
+    template: `
+	<div class = "news">
+	     <h2> News </h2>
+	     <ul class = "news__list" style = "list-style-type: none">
+		<div class = "grid">
+		<li v-for = "article in articles" class = "news__item">
 
-let app = new Vue({
-    el: '#app',
-    data: {
-        welcome: 'Hello World! Welcome to VueJS'
+		<div class = "card">
+	     	<div class = "card-body">
+		 <h5 class = "card-title"> {{article.title}} </h5>
+		 <img class = "card-img-top" src =  "/static/images/Placeholder Image.png"/>
+		<p>
+		 {{ article.description}}
+		</p>
+		</div>
+	   	</div>
+		 </li>
+		</div>
+		
+	     </ul>
+	<div class = "form-inline d-flex justify-content-center">
+	    <div class = "form-group mx-sm-3 mb-2">
+		<label class = "sr-only" for = "search"> Search </label>
+		<input type ="search" name = "search" v-model="searchTerm" id = "search" class = "form-control mb-2 mr-sm-2" placeholder = "Enter search term here" />
+		
+		<button class = "btn btn-primary mb-2" @click= "searchNews"> Search  </button>
+	    </div>
+	</div>
+	</div>`,
+
+    created: function(){
+	let self = this;
+	fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=3be87baac222432f8cc08e8cd071f1d8').then(function(response){
+		
+		return response.json();
+		}).then (function(data){
+		
+		console.log(data);
+		self.articles = data.articles;
+		});
+	},
+	data: function(){
+		return {
+		   articles: [],
+		   searchTerm: ''
+		}
+	},
+	methods: {
+	   searchNews: function(){
+	   let self = this;
+	   fetch('https://newsapi.org/v2/everything?q=' + self.searchTerm + '&hlanguages=en&apiKey=3be87baac222432f8cc08e8cd071f1d8').then(function(response){
+		
+		return response.json();
+		}).then (function(data){
+		
+		console.log(data);
+		self.articles = data.articles;
+		});
+	}
+
+     }
+	
+});
+
+const Home = Vue.component('home',{
+    template:`
+	<div class = "home">
+	<img src = "/static/images/logo.png" alt = "VueJS Logo">
+	<h1> {{ welcome }} </h1>
+	</div>
+	`,
+    data: function(){
+	return {
+	    welcome: 'Hello World! Welcome to VueJs'
+
+	}
     }
+
+});
+
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+	{ path:'/' , component: Home },
+	{ path: '/news', component: NewsList }
+
+    ]
+
+});
+
+
+
+
+
+const app = new Vue({
+    el: '#app',
+    router
 });
 
